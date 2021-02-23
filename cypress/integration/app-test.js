b826374ddd49ba87f26cf0cb89074e4146400019
JuissiJohtaja <juha.koivu@outlook.com>
2021-02-23 14:23:22 +0200

    test

--- cypress/integration/app-test.js
+++ cypress/integration/app-test.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-undef */
 describe('Anecdote App', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
+    //cy.visit('http://localhost:3000')
     cy.contains('Anecdotes')
     cy.contains('Debugging is twice as hard as writing the code in the first place.')
   })
 
   it('Vote can be clicked', function() {
-    cy.visit('http://localhost:3000')
+    //cy.visit('http://localhost:3000')
     cy.contains('vote').click()
     cy.contains('You voted')
   })
