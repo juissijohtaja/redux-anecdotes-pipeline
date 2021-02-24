1d0e096b1252613e7b5b6bb1065aac5a7fa17f70
JuissiJohtaja <juha.koivu@outlook.com>
2021-02-24 17:11:46 +0200

    test

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./server/anecdotes.json')
@@ -7,4 +8,31 @@ const port = process.env.PORT || 3000
 server.use(middlewares)
 server.use(router)
 
+const express = require('express')
+const path = require('path')
+server.use(express.static(path.join(__dirname, 'build2')))
+server.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
 server.listen(port)
+
+
+/*
+const jsonServer = require('json-server')
+const app = jsonServer.create()
+const path = require('path')
+const express = require('express')
+const middlewares = jsonServer.defaults()
+const router = jsonServer.router('./server/anecdotes.json')
+const port = process.env.PORT || 3000
+
+app.use('/anecdotes', middlewares, router)
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(port)
+ */
